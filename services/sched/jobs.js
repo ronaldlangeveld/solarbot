e5eddfcc4c50e088473ef88ee1ea92f8ecbf9376
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-06-26 17:31:34 +0200

    changed schedule to be every 4 minutes due to api constraints

--- services/sched/jobs.js
+++ services/sched/jobs.js
@@ -3,7 +3,7 @@ const { testPower } = require('../../services/tasks/testPower');
 const {allDataMessage} = require('../telegram_bot/templates');
 const {sendTelegramMessage} = require('../telegram_bot/index');
 
-const updateSolarData = cron.schedule('*/1 * * * * ', async () => {
+const updateSolarData = cron.schedule('*/4 * * * * ', async () => {
     const {gridFrequencyLatest, gridFrequencyNow, batteryLevelNow, sunPower, consumptionNow, hasChanged} = await testPower();
     const message = allDataMessage(hasChanged.isOn, batteryLevelNow, sunPower, consumptionNow);
     if(hasChanged.gridChange){
